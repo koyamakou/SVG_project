d8cdb2e8e187e6c3fbee36b1bab33135baab917c
koyamakou <mahaisodain@gmail.com>
2020-01-27 22:39:55 +0900

    I finished svg that drawing by anime.js

--- hp/javascript/log.js
+++ hp/javascript/log.js
@@ -1,53 +1,45 @@
 $(function(){
-	$("#鬼").load('picture/鬼.svg svg', function(){
+	$("#SVG_Data").load('picture/鬼.svg svg', function(){
 		let svgId;
-		/*for(let i=1; i<5; i++){
-			svgId = document.querySelectorAll('#svg'+i);
-			SvgAnime(svgId);
-		};*/
-		svgId = document.querySelectorAll('#svg1');
+		svgId = document.querySelectorAll('.svg');
 		SvgAnime(svgId);
 
 		console.log('aaaaaaaaaaa');
 	});
 });
 
-
-
 var SvgAnime = function(childPath){
 	console.log('SvgAnime-始まり');
-	let dashoffset = childPath[0].getTotalLength()*2;
-	anime({
-		targets: childPath,
-		strokeDashoffset: [anime.setDashoffset, dashoffset],
-		easing: 'easeInOutSine',
-		duration: 2000,
-		delay: function(el, i) { return i*250 },
-		direction: 'normal',
+	const logoAnimationTL = anime.timeline({
+    autoplay: false,
+    easing: 'easeInOutQuint',
+  })
+	.add({
+		targets: childPath[0],
+		strokeDashoffset: [anime.setDashoffset, childPath[0].getTotalLength()*2],
+		duration: 1000,
+		//direction: 'alternate',
 		loop: false,
-	});
-	// CSSPropertyの取得
-	let style = window.getComputedStyle(childPath[0], null).getPropertyValue('stroke-dashoffset');
-
-	// 文字列を数値化(今回は少数まで出す必要があるため、float)
-	let number = parseFloat(style, 10);
-	console.log(number);
-	console.log(dashoffset);
-/*
-	while(number < dashoffset ){
-		style = window.getComputedStyle(childPath[0], null).getPropertyValue('stroke-dashoffset');
-		number = parseFloat(style, 10);
-
-		console.log(number);
-		if(number > dashoffset){
-			console.log("11111111111111aa"+number);
-			break;
-		}
-	}
-*/
-
-//	console.log(style);
-
+	})
+	.add({
+		targets: childPath[1],
+		strokeDashoffset: [anime.setDashoffset, childPath[1].getTotalLength()*2],
+		duration: 1000,
+		loop: false,
+	},'-=600')
+	.add({
+		targets: childPath[2],
+		strokeDashoffset: [anime.setDashoffset, childPath[2].getTotalLength()*2],
+		duration: 1000,
+		loop: false,
+	},'-=600')
+	.add({
+		targets: childPath[3],
+		strokeDashoffset: [anime.setDashoffset, childPath[3].getTotalLength()*2],
+		duration: 1000,
+		loop: false,
+	},'-=600');
 
 	console.log('SvgAnime-終わり');
+	logoAnimationTL.play();
 }
