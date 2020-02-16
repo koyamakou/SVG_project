576815fbf3dcc1f59be633ff3d0745c82a4f8c60
koyamakou <mahaisodain@gmail.com>
2020-02-16 23:06:07 +0900

    animetion's base

--- hp/javascript/module.js
+++ hp/javascript/module.js
@@ -1,4 +1,4 @@
-
+/**SVGを動かす*/
 
 export const SvgAnime = function(childPath){
   console.log('SvgAnime-始まり');
@@ -9,7 +9,7 @@ export const SvgAnime = function(childPath){
     loop: false
   })
   /*SVGを視覚化(文字が手書き風に出力される)*/
-  logoAnimationTL
+  /*logoAnimationTL
   .add({
     targets: childPath[0],
     strokeDashoffset: [anime.setDashoffset, childPath[0].getTotalLength()*2],
@@ -34,7 +34,7 @@ export const SvgAnime = function(childPath){
   logoAnimationTL.play();
 
   /*各要素のdurationや秒数の指定＆配列化*/
-  let animeId = [
+/*  let animeId = [
     //{Id: childPath[0], duration: 1100},
     {Id: childPath[0], duration: 1100, second: '-=200'},
     {Id: childPath[1], duration: 900, second: '-=900'},
@@ -44,37 +44,76 @@ export const SvgAnime = function(childPath){
 
   /*配列を回して、fill-opacityを1に変更。これにより、あらかじめ
   指定していた色が浮き上がる*/
-  animeId.forEach(function(Path, index) {
+  /*animeId.forEach(function(Path, index) {
     logoAnimationTL
     .add({
       targets: Path.Id,
-      /*	fill: {
+      /*廃止	fill: {
       //value: ['#ffffff', '#ff0000'],
       value: ['#000000','url(#g1)'],
       duration: Path.duration,
       easing:'easeInSine',
     },*/
-    duration: Path.duration,
+/*    duration: Path.duration,
     easing:'easeInSine',
     fillOpacity: 1,
   },Path.second)
 });
 /*opacityを使って不可視化*/
 logoAnimationTL
-.add({
+/*.add({
   targets: childPath,
   duration: 850,
   delay: 350,
   opacity: 0,
   easing: 'easeInCubic',
+})
+/*DOM操作*/
+.add({
   complete: function(){
-    var obj = document.getElementById('SVG_Data');
-    obj.id = 'bacuk_picture';
+    // id:SVG_Dataの削除
+    let obj = document.getElementById('SVG_Data');
+    obj.remove();
+
+    // オープニングシャッターのrootクラス
+    // bacuk_pictureの追加
+    let divId = document.createElement('div');
+    divId.className = 'back_picture';
+    document.querySelector('.op_animetion').appendChild(divId);
+
+    // cssAnimetionのオープニングシャッター用のdiv作成
+    let fragment = document.createDocumentFragment();
+    let createChildDiv;
+
+    // シャッターアニメーション開始用div
+    createChildDiv = document.createElement('div');
+    createChildDiv.className = 'animetion first_start';
+    fragment.appendChild(createChildDiv);
+
+    // 画像表示用div作成
+    for (var i = 1; i < 8; i++) {
+      createChildDiv = document.createElement('div');
+      createChildDiv.className = 'animetion picture_' + i;
+      fragment.appendChild(createChildDiv);
+  };
+    document.querySelector('.back_picture').appendChild(fragment);
   }
-});
+}, '-=650');
+/*
+.add ({
+  complete: function(){
+
+    // cssAnimetionのオープニングシャッター用のdiv作成
+    let fragment = document.createDocumentFragment();
+    let creatediv;
+    for (var i = 1; i < 3; i++) {
+      creatediv = document.createElement('div');
+      creatediv.className = 'shuter' + i;
+      fragment.appendChild(creatediv);
+  };
+    document.querySelector('.picture_1').appendChild(fragment);
+  }
+});*/
+
 logoAnimationTL.play();
-/*	if(){
-var obj = document.getElementById('SVG_Data');
-obj.id = 'bacuk_picture';
-}*/
 }
